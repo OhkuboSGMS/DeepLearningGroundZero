6a59bf2c6cffca07e4cd689560daca5a6add530d
OhkuboSGMS <sinikake.gokiburi@gmail.com>
2017-04-17 22:41:32 +0900

    finish chap4

---
+++ Layer.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        out = x * y
+
+        return out
+
+    def backward(self, dout):
+        dx = dout * self.y
+        dy = dout * self.x
+
+        return dx, dy
+
+
+class AddLayer:
+    def __init__(self):
+        pass
+
+    def forward(self, x, y):
+        out = x + y
+        return out
+
+    def backward(self, dout):
+        dx = dout * 1
+        dy = dout * 1
+        # タプルで返却
+        return (dx, dy)
+
+
+class ReLuLayer:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+
+        return out
+
+    def backward(self, dout):
+        # self.mask[X]=Trueのところを0にする
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+
+
+class SigmoidLayer:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+
+        return dx
