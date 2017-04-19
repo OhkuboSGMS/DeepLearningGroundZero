035e0ccc2f1edd70f1b1504e34ac4965f4e75bed
OhkuboSGMS <sinikake.gokiburi@gmail.com>
2017-04-19 19:38:29 +0900

    finish chap5

--- Differentiation.py
+++ Differentiation.py
@@ -1,3 +1,4 @@
+import numpy as np
 def numerical_diff(f, x):
     h = 1e-4
     return (f(x + h) - f(x - h)) / (2 * h)
@@ -7,19 +8,19 @@ def numerical_gradient(f, x):
     h = 1e-4
     # xと同じサイズの0埋めの配列を生成
     grad = np.zeros_like(x)
-
-    for idx in range(x.size):
+    it = np.nditer(x, flags=["multi_index"], op_flags=["readwrite"])
+    while not it.finished:
+        idx = it.multi_index
         tmp_val = x[idx]
-
-        x[idx] = tmp_val + h
+        x[idx] = float(tmp_val) + h
         fxh1 = f(x)
-        print(fxh1)
 
         x[idx] = tmp_val - h
         fxh2 = f(x)
 
         grad[idx] = (fxh1 - fxh2) / (2 * h)
         x[idx] = tmp_val
+        it.iternext()
 
     return grad
 
